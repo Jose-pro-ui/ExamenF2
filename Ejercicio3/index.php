6e5460b65fb3a44758a9915408f7cde17be8e94c
Jose Aguilar <chino600celeste@gmail.com>
2020-11-20 02:53:24 -0400

    nuevo index

--- Ejercicio3/index.php
+++ Ejercicio3/index.php
@@ -7,177 +7,73 @@
     <link rel="icon" href="img/logo.png"/>
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./crud_ajax/jquery-ui.css">
+    <link rel="stylesheet" href="./crud_ajax/bootstrap.min.css" />
 </head>
 <body>
-  <!--CABECERA-->
-  <header class="header">  
-    <ul>
-      <li><img src="img/logo.png" class="logo"></li>
-      <li><a href="index.html">Inicio</a></li>
-      <li><a href="#nosotros">Nosotros</a></li>                
-      <li><a href="#productos">Productos</a></li>
-      <li><a href="#contactos">Contactos</a></li>
-    </ul>
-  </header>
+    <!--CABECERA-->
+    <header class="header">  
+        <ul>
+        <li><img src="img/logo.png" class="logo"></li>
+        <li><a href="index.html">Inicio</a></li>
+        <li><a href="#nosotros">Nosotros</a></li>                
+        <li><a href="#productos">Productos</a></li>
+        <li><a href="#contactos">Contactos</a></li>
+        </ul>
+    </header>
 
-  <!--PORTADA-->
-  <div class="portada">
-    <img src="img/portada.png" alt="" class="imgpor">
-  </div>
-
-  <!--NOSOTROS-->
-  <h1 class="titulo" id="nosotros">NOSOTROS</h1>
-  <section class="nosotros">
-    <div class="box">
-      <img src="img/mision.png" class="imgvm">
-      <h2 class="t2">Mision</h2>
-      <p class="p2">Elaborar y comercializar productos lácteos de la más alta calidad que contribuyan al crecimiento y nutrición de una población saludable.</p>
+    <!--PORTADA-->
+    <div class="portada">
+        <img src="img/portada.png" alt="" class="imgpor">
     </div>
 
-    <div class="box">
-      <img src="img/vision.png" class="imgvm">
-      <h2 class="t2">Vision</h2>
-      <p class="p2">Ser la empresa de industrias lácteas líder en el mercado centroamericano al satisfacer las necesidades alimenticias de la población ofreciéndoles siempre productos de primera calidad, manteniendo un enfoque en el apoyo a la sociedad.</p>
+    <!--PRODUCTOS-->
+    <h1 class="titulo" id="productos">PRODUCTOS</h1>
+    <div class="container">
+		<br />	
+        
+        <h3 align="center"> Mis Productos</a></h3><br />
+        
+        <br />
+        
+		<div align="right" style="margin-bottom:5px;">
+		    <button type="button" name="add" id="add" class="btn btn-success btn-xs">Añadir</button>
+		</div>
+        
+        <div class="table-responsive" id="user_data"></div>
+		<br />
     </div>
-  </section>
-
-  <!--PRODUCTOS-->
-  <h1 class="titulo" id="productos">PRODUCTOS</h1>
-  <section class="productos">
-    <div class="producto">
-      <img src="img/img1.jpg" class="producto-img">
-      <div class="producto-info">
-        <h3>Yogur con sabor a fresa</h3>
-        <p>
-          Los Danone sabores de toda <br>
-          la vida, sabor fresa.
-        </p>
-      </div>
-      <div class="btn">
-        <a href="./crud_ajax/index.php"><button type="submit">Añadir</button></a>
-      </div>
-    </div>
-
-    <div class="producto">
-      <img src="img/img2.jpg" class="producto-img">
-      <div class="producto-info">
-        <h3>Yogur cremoso con fresa</h3>
-        <p>
-          Los yogures Originales vienen en <br>
-          envases de cristal y con Fresas.
-        </p>
-      </div>
-      <div class="btn">
-        <a href="./crud_ajax/index.php"><button type="submit">Añadir</button></a>
-      </div>
-    </div>
-
-    <div class="producto">
-      <img src="img/img3.jpg" class="producto-img">
-      <div class="producto-info">
-        <h3>Yogur con sabor a limón</h3>
-        <p>
-          Este Yogur con sabor a limón ha <br>
-          sido elaborado con leche fresca.
-        </p>
-      </div>
-      <div class="btn">
-        <a href="./crud_ajax/index.php"><button type="submit">Añadir</button></a>
-      </div>
-    </div>
-
-    <div class="producto">
-      <img src="img/img4.jpg" class="producto-img">
-      <div class="producto-info">
-        <h3>Yogures sabores variados</h3>
-        <p>
-          Un surtido de yogures sabores <br>
-          Plátano, Macedonia, Fresa y <br>
-          Tarta de Queso.
-        </p>
-      </div>
-      <div class="btn">
-        <a href="./crud_ajax/index.php"><button type="submit">Añadir</button></a>
-      </div>
-    </div>
-
-    <div class="producto">
-      <img src="img/img5.jpg" class="producto-img">
-      <div class="producto-info">
-        <h3>Yogur cremoso de Jumilla</h3>
-        <p>
-          Este Yogur cremoso esta hecho <br>
-          con trozos de peras de Jumilla <br>
-          nutritivos.
-        </p>
-      </div>
-      <div class="btn">
-        <a href="./crud_ajax/index.php"><button type="submit">Añadir</button></a>
-      </div>
-    </div>
-
-    <div class="producto">
-      <img src="img/img6.jpg" class="producto-img">
-      <div class="producto-info">
-        <h3>Yogures Sutidos</h3>
-        <p>
-          Un surtido de yogures Danone <br>
-          sabores Fresa, Plátano, <br>
-          Macedonia y Vainilla.
-        </p>
-      </div>
-      <div class="btn">
-        <a href="./crud_ajax/index.php"><button type="submit">Añadir</button></a>
-      </div>
-    </div>
-  </section>
-
-  <!--COTACTOS-->
-  <h1 class="titulo" id="contactos">CONTACTOS</h1>
-  <div class="formulario">
-    <form action="">
-
-      <div class="name">
-        <label for="">Nombre Completo:</label>
-        <input type="text">
-      </div>
-
-      <div class="email">
-        <label for="">Correo Electronico:</label>
-        <input type="email">
-      </div>
-
-      <div class="phone">
-        <label for="">Celular:</label>
-        <input type="number">
-      </div>
-
-      <div class="message">
-        <label for="">Comentario:</label>
-        <textarea type="text"></textarea>
-      </div>
-
-      <button type="submit">Enviar</button>
-    </form>
-  </div>
-
-  <!--FOOTER-->
-  <footer>
-    <div class="contact">
-      <div class="box">
-        <h2 class="t4">CONTACTANOS</h2>
-        <div class="contact-info">
-          <span> <i class="fas fa-phone"></i> 2 224567</span>
-          <span> <i class="fas fa-envelope"></i> info@donane.gmail.com</span>
-          <span> <i class="fas fa-map-marker-alt"></i> Av. 16 de Julio</span>
-          <span> <i class="fab fa-whatsapp"></i> 75874590</span>
+    
+	<div id="user_dialog" title="Registro de Producto">
+		<form method="post" id="user_form">
+			<div class="form-group">
+				<label>Ingrese el producto</label>
+				<input type="text" name="producto" id="producto" class="form-control" />
+				<span id="error_producto" class="text-danger"></span>
+			</div>
+			<div class="form-group">
+				<label>Ingrese el precio</label>
+				<input type="text" name="precio" id="precio" class="form-control" />
+				<span id="error_precio" class="text-danger"></span>
+			</div>
+			<div class="form-group">
+				<input type="hidden" name="action" id="action" value="insert" />
+				<input type="hidden" name="hidden_id" id="hidden_id" />
+				<input type="submit" name="form_action" id="form_action" class="btn btn-info" value="Insert" />
+			</div>
+		</form>
+	</div>
+		
+	<div id="action_alert" title="Action"></div>
+		
+	<div id="delete_confirmation" title="Confirmation">
+		<p>Esta seguro de Eliminar es elemento?</p>
+	</div>
+    <!--FOOTER-->
+    <footer>
+        <div class="derechos">
+        <h4>Derechos Reservados © Danone</h4>
         </div>
-      </div>
-    </div>
-
-    <div class="derechos">
-      <h4>Derechos Reservados © Danone</h4>
-    </div>
-  </footer>
+    </footer>
 </body>
 </html>
